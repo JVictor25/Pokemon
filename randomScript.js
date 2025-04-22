5665d1732bd60804cce7385fb5fa81c20ae2837d
JVictor <joaovictorguedes2003@hotmail.com>
2025-04-22 16:59:54 -0300

    btnRandomizar

--- randomScript.js
+++ randomScript.js
@@ -1,9 +1,9 @@
-const pokemonContainer = document.querySelector("#pokeContainer")
-const searchInput = document.getElementById('search-input');
+const pokemonContainer = document.querySelector("#teamContainer");
+const btnRandomizar = document.getElementById("btnRandomizar");
 
-pokemonCounter= 1025;
+pokemonCounter = 1025;
 
-colors = {
+const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
     electric: '#FCF7DE',
@@ -22,20 +22,9 @@ colors = {
     ghost: '#C6B7F5',
     ice: '#e0f5ff',
     steel: '#d1d1e0'
-}
+};
 
 const mainTypes = Object.keys(colors);
-
-const fetchPokemons = async () => {
-    for (let i = 1; i <= pokemonCounter; i++) {
-        await getPokemonData(i);
-    }
-}
-
-const randomPokemon = () => {
-    const randomNumber = Math.floor(Math.random() * pokemonCounter) + 1;
-    getPokemonData(randomNumber);
-}
 
 const getPokemonData = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -44,7 +33,6 @@ const getPokemonData = async (id) => {
         throw new Error('Pokemon not found');
     }
     const data = await response.json();
-    console.log(data);
     createPokemonCard(data);
 }
 
@@ -55,32 +43,38 @@ const createPokemonCard = (pokemon) => {
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
     const id = pokemon.id.toString().padStart(3, '0');
     const pokeTypes = pokemon.types.map(type => type.type.name);
-    const type =mainTypes.find(type => pokeTypes.indexOf(type) > -1);
+    const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1);
     const color = colors[type];
     const Height = pokemon.height / 10;
-    mt = "";
-    if (Height >= 2){
-        mt = " Mts";}
-    if(Height < 2){mt = " M";}
-    
     const Weight = pokemon.weight / 10;
+    const mt = Height >= 2 ? " Mts" : " M";
 
     pokemonEl.style.backgroundColor = color;
 
-    const pokemonInnerHTML = `<div class="pokeCard" id="pokeCard">
-                <h2 id="pokeName">${name.toUpperCase()}</h2>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="Pikachu" class="pokeImage" id="pokeImage">
-                <p id="pokeId"><span id="pokeIdNumber">#${id}</span></p>
-                <p id="pokeType">Type: ${type.toUpperCase()}</p>
-                <p id="pokeHeight">Height: ${Height}${mt}</p>
-                <p id="pokeWeight">Weight: ${Weight} KG</p>
-            </div>`
-            pokemonEl.innerHTML = pokemonInnerHTML;
-            pokemonContainer.appendChild(pokemonEl);    
-    
+    const pokemonInnerHTML = `
+        <div class="pokeCard">
+            <h2>${name.toUpperCase()}</h2>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" class="pokeImage">
+            <p><span>#${id}</span></p>
+            <p>Type: ${type.toUpperCase()}</p>
+            <p>Height: ${Height}${mt}</p>
+            <p>Weight: ${Weight} KG</p>
+        </div>`;
+
+    pokemonEl.innerHTML = pokemonInnerHTML;
+    pokemonContainer.appendChild(pokemonEl);
 }
 
+const randomTeam = () => {
+    pokemonContainer.innerHTML = '';
+    const usedIds = new Set();
+    while (usedIds.size < 6) {
+        const randomId = Math.floor(Math.random() * pokemonCounter) + 1;
+        if (!usedIds.has(randomId)) {
+            usedIds.add(randomId);
+            getPokemonData(randomId);
+        }
+    }
+}
 
-
-//fetchPokemons();
-randomPokemon();
+btnRandomizar.addEventListener("click", randomTeam);
